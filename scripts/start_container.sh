08de0a04b52ea7590b2a9a6e14da2a99f02921b0
Suresh0550 <sureshreddy.rapolu4@gmail.com>
2024-12-14 00:13:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull suresh6784/simple-python-flask-app
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 suresh6784/simple-python-flask-app
 
