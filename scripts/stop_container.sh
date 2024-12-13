ec84540c96b12f4c7cdd6d68cc0e331ee3fd4540
Suresh0550 <sureshreddy.rapolu4@gmail.com>
2024-12-14 02:18:01 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerID=`docker ps | awk -F " " '{print $1}'`
-docker rm -F $containerID
+containerID=`docker ps | awk -f " " '{print $1}'`
+docker rm -f $containerID
