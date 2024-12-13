f43ceccc41c2ce703f8fb1580cd45940b9b2d802
Suresh0550 <sureshreddy.rapolu4@gmail.com>
2024-12-14 02:54:49 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-set -e
+#!/bin/bash
 
-# Stop the running container (if any)
-docker rm -v -f $(docker ps -qa)
+# Get all container IDs (including stopped ones)
+containerID=$(docker ps -qa)
+
+if [ -n "$containerID" ]; then
+    # Remove all containers and associated volumes
+    docker rm -v -f $containerID
+    echo "All containers and their associated volumes have been removed."
+else
+    echo "No containers to remove."
+fi
 
 
